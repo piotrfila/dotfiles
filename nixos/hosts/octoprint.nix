c3537f200e0643cbc642119434a028e9dadb7ea5
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-06-16 19:01:44 +0200

    added octoprint raspberry pi server

---
+++ nixos/hosts/octoprint.nix
@@ -0,0 +1,92 @@
+{ config, lib, pkgs, modulesPath, ... }: {
+  imports = [
+    ../boot/extlinux.nix
+    ../cli.nix
+    ../locale/pl.nix
+    ../network/systemd-wireless.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    initrd.availableKernelModules = [ "xhci_pci" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+  };
+
+  environment.etc.machine-id.text = "326361ad04094a9b86bd130186912b6f\n";
+
+  fileSystems = {
+    "/" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+      options = [ "size=32G" "mode=755" ];
+    };
+    "/nix" = {
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
+  } // (builtins.listToAttrs (
+    builtins.map ( x: {
+      name = x;
+      value = {
+        device = "/nix/persist${x}";
+        fsType = "none";
+        options = [ "bind" ];
+      };
+    }) [
+      "/root"
+      "/home"
+      #"/var/log"
+      #"/var/lib/nixos"
+      #"/var/lib/systemd/coredump"
+    ]
+  ));
+
+  networking.hostName = "octoprint";
+  # networking.interfaces.end0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
+  networking.wireless.enable = true;
+
+  nixpkgs.hostPlatform = "aarch64-linux";
+
+  services = {
+    getty.autologinUser = "printer";
+    openssh.enable = true;
+  };
+
+  programs = {
+    fish.enable = true;
+  };
+
+  system.stateVersion = "24.05";
+
+  systemd.network.links = {
+    "10-eth0" = {
+      matchConfig.MACAddress = "e4:5f:01:4b:cb:7a";
+      linkConfig.Name = "eth0";
+    };
+    "15-wlan0" = {
+      matchConfig.MACAddress = "e4:5f:01:4b:cb:7b";
+      linkConfig.Name = "wlan0";
+    };
+  };
+
+  swapDevices = [ ];
+
+  users.mutableUsers = false;
+  users.users.printer = {
+    extraGroups = [ "wheel" ];
+    hashedPasswordFile = "/nix/persist/home/printer/passwd";
+    isNormalUser = true;
+    packages = [ pkgs.git ];
+    shell = pkgs.fish;
+  };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 75;
+  };
+}
