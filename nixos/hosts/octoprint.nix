9666a14055e7b04252004cbd1129e888e63ef3ec
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-06-16 20:13:25 +0200

    add klipper and move it to a separate file

--- nixos/hosts/octoprint.nix
+++ nixos/hosts/octoprint.nix
@@ -4,6 +4,7 @@
     ../cli.nix
     ../locale/pl.nix
     ../network/systemd-wireless.nix
+    ../various/klipper/default.nix
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
@@ -57,45 +58,6 @@
   services = {
     getty.autologinUser = "printer";
     openssh.enable = true;
-
-    klipper = {
-      # enable = true;
-      # firmwares = {
-      #   mcu = {
-      #     enable = true;
-      #     Run klipper-genconf to generate this
-      #     configFile = ./avr.cfg;
-      #     Serial port connected to the microcontroller
-      #     serial = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_55639303235351D01152-if00";
-      #   };
-      # };
-    };
-    moonraker = {
-      address = "0.0.0.0";
-      enable = true;
-      settings = {
-        octoprint_compat = { };
-        history = { };
-        authorization = {
-          force_logins = true;
-          cors_domains = [
-            "*.local"
-            "*.lan"
-            "*://app.fluidd.xyz"
-            "*://my.mainsail.xyz"
-          ];
-          trusted_clients = [
-            "127.0.0.0/8"
-            "192.168.1.0/24"
-          ];
-        };
-      };
-      user = "printer";
-    };
-
-    mainsail = {
-      enable = true;
-    };
   };
 
   programs.fish.enable = true;
---
+++ nixos/various/klipper/default.nix
@@ -0,0 +1,43 @@
+{ ... }: {
+  services = {
+    klipper = {
+      enable = true;
+      configFile = ./printer
+      # firmwares = {
+      #   mcu = {
+      #     enable = true;
+      #     Run klipper-genconf to generate this
+      #     configFile = ./avr.cfg;
+      #     Serial port connected to the microcontroller
+      #     serial = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_55639303235351D01152-if00";
+      #   };
+      # };
+    };
+    moonraker = {
+      address = "0.0.0.0";
+      enable = true;
+      settings = {
+        octoprint_compat = { };
+        history = { };
+        authorization = {
+          force_logins = true;
+          cors_domains = [
+            "*.local"
+            "*.lan"
+            "*://app.fluidd.xyz"
+            "*://my.mainsail.xyz"
+          ];
+          trusted_clients = [
+            "127.0.0.0/8"
+            "192.168.1.0/24"
+          ];
+        };
+      };
+      user = "printer";
+    };
+
+    mainsail = {
+      enable = true;
+    };
+  };
+}
