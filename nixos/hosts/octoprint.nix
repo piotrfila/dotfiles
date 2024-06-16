5d6921135f280c28e0b631480a4a8685eac8b763
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-06-16 19:50:56 +0200

    add moonraker

--- nixos/hosts/octoprint.nix
+++ nixos/hosts/octoprint.nix
@@ -70,6 +70,28 @@
       #   };
       # };
     };
+    moonraker = {
+      address = "0.0.0.0";
+      enable = true;
+      settings = {
+        octoprint_compat = { };
+        history = { };
+        authorization = {
+          force_logins = true;
+          cors_domains = [
+            "*.local"
+            "*.lan"
+            "*://app.fluidd.xyz"
+            "*://my.mainsail.xyz"
+          ];
+          trusted_clients = [
+            "127.0.0.0/8"
+            "192.168.1.0/24"
+          ];
+        };
+      };
+      user = "printer";
+    };
 
     mainsail = {
       enable = true;
