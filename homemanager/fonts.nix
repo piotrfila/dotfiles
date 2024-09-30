b963cdbd404e2f5eb86863b3ce14caa517ca25df
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-09-30 18:17:37 +0200

    missing file

---
+++ homemanager/fonts.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  home.packages = [ (pkgs.nerdfonts.override { fonts = ["FiraCode"];}) ];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = ["FiraCode Nerd Font Mono"];
+      serif = ["Noto Serif" "Garamond"];
+      sansSerif = ["Noto Sans"];
+    };
+  };
+}
