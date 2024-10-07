bf3549e9a66568c6e800bc0e8ff3197cf6fe30a1
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-10-07 18:15:21 +0200

    fixing adguard settings part 2

--- nixos/various/adguard.nix
+++ nixos/various/adguard.nix
@@ -3,26 +3,26 @@
     enable = true;
     settings = {
       https.address = "127.0.0.1:3003";
-    };
-    dns.upstream_dns = [
-      # Example config with quad9
-      "9.9.9.9#dns.quad9.net"
-      "149.112.112.112#dns.quad9.net"
-    ];
-    filtering = {
-      protection_enabled = true;
-      filtering_enabled = true;
-
-      parental_enabled = false;
-      safe_search.enabled = false;
-    };
-    filters =
-      map (url: {
-        enabled = true;
-        url = url;
-      }) [
-        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
-        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+      dns.upstream_dns = [
+        # Example config with quad9
+        "9.9.9.9#dns.quad9.net"
+        "149.112.112.112#dns.quad9.net"
       ];
+      filtering = {
+        protection_enabled = true;
+        filtering_enabled = true;
+
+        parental_enabled = false;
+        safe_search.enabled = false;
+      };
+      filters =
+        map (url: {
+          enabled = true;
+          url = url;
+        }) [
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+        ];
+    };
   };
 }
