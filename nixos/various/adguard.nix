e4c19e08465e1fb045ae53ff3f15e6e6cc649dcb
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-10-07 18:14:06 +0200

    fixing adguard settings

--- nixos/various/adguard.nix
+++ nixos/various/adguard.nix
@@ -9,20 +9,20 @@
       "9.9.9.9#dns.quad9.net"
       "149.112.112.112#dns.quad9.net"
     ];
-  };
-  filtering = {
-    protection_enabled = true;
-    filtering_enabled = true;
+    filtering = {
+      protection_enabled = true;
+      filtering_enabled = true;
 
-    parental_enabled = false;
-    safe_search.enabled = false;
+      parental_enabled = false;
+      safe_search.enabled = false;
+    };
+    filters =
+      map (url: {
+        enabled = true;
+        url = url;
+      }) [
+        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
+        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+      ];
   };
-  filters =
-    map (url: {
-      enabled = true;
-      url = url;
-    }) [
-      "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
-      "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
-    ];
 }
