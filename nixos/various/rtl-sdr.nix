d9bbb120a1257d93e92f2631bc0ea1754cae7cce
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-11-14 19:10:42 +0100

    rtl-sdr package for debug

--- nixos/various/rtl-sdr.nix
+++ nixos/various/rtl-sdr.nix
@@ -2,4 +2,5 @@
   boot.blacklistedKernelModules = ["dvb_usb_rtl28xxu"];
   hardware.rtl-sdr.enable = true;
   services.udev.packages = [pkgs.rtl-sdr];
+  environment.systemPackages = [pkgs.rtl-sdr];
 }
