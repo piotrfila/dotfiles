0e99a2bffb64bee808880af376cf95d443051538
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2024-11-14 19:10:20 +0100

    libraries for factorio

--- nixos/various/nix-ld.nix
+++ nixos/various/nix-ld.nix
@@ -2,6 +2,19 @@
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
+      alsa-lib
+      libGL
+      xorg.libICE
+      xorg.libSM
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXext
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXrandr
+      libpulseaudio
+      libxkbcommon
+      wayland
     ];
   };
 }
