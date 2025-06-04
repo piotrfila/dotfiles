89959ec84f608d36fee64126cd07c91032f760ee
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2025-06-04 22:01:52 +0200

    run alejandra

--- scripts/wpctl-current-sink.nix
+++ scripts/wpctl-current-sink.nix
@@ -1,22 +1,22 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     wireplumber
-    (writeShellScriptBin  "wpctl-current-sink" ''
-    current_sink=$(\
-      wpctl status | \
-      grep "^ │  \*" | \
-      head -n 1 \
-    )
-    
-    if [ $1 = "is-muted" ]; then
-      if [ "''\${current_sink/"MUTED]"//}" = "$current_sink" ]; then
-        echo "0"
+    (writeShellScriptBin "wpctl-current-sink" ''
+      current_sink=$(\
+        wpctl status | \
+        grep "^ │  \*" | \
+        head -n 1 \
+      )
+
+      if [ $1 = "is-muted" ]; then
+        if [ "''\${current_sink/"MUTED]"//}" = "$current_sink" ]; then
+          echo "0"
+        else
+          echo "1"
+        fi
       else
-        echo "1"
+        wpctl $1 ''\${current_sink:8:2} $2
       fi
-    else
-      wpctl $1 ''\${current_sink:8:2} $2
-    fi
     '')
   ];
 }
