d8bbd31f56a1bb2a3ceb3fa78c3ffd6db8be75a3
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2025-08-13 05:20:44 +0200

    update to gimp 3

--- homemanager/gimp.nix
+++ homemanager/gimp.nix
@@ -5,12 +5,12 @@
 }: let
   util = import ../util.nix;
 in {
-  # home.file = util.persist {
-  #   inherit config;
-  #   symlinks = [
-  #     ".config/GIMP"
-  #     ".local/share/gegl-0.4"
-  #   ];
-  # };
-  home.packages = [pkgs.gimp];
+  home.file = util.persist {
+    inherit config;
+    symlinks = [
+      ".config/GIMP"
+      ".local/share/gegl-0.4"
+    ];
+  };
+  home.packages = [pkgs.gimp3];
 }
