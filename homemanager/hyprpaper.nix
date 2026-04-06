e79d704d268be2f9d231af49bf141bcf1e5c0c8e
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2026-04-06 22:29:43 +0200

    wallpaper change

--- homemanager/hyprpaper.nix
+++ homemanager/hyprpaper.nix
@@ -2,19 +2,19 @@
   services.hyprpaper = {
     enable = true;
     settings = let
-      wp = "~/Pictures/wallpapers";
+      wp = "~/Pictures/wallpapers/artemis.jpg";
     in {
       ipc = "off";
       preload = [
-        "${wp}/train.png"
+        "${wp}"
       ];
       splash = "false";
       wallpaper = [
-        "eDP-1,${wp}/train.png"
-        "DP-1,${wp}/train.png"
-        "DP-2,${wp}/train.png"
-        "HDMI-A-1,${wp}/train.png"
-        "HDMI-A-2,${wp}/train.png"
+        "eDP-1,${wp}"
+        "DP-1,${wp}"
+        "DP-2,${wp}"
+        "HDMI-A-1,${wp}"
+        "HDMI-A-2,${wp}"
       ];
     };
   };
