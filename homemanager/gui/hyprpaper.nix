5f3caa6f78de8854ea6826048a586cefb56d2c20
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2026-05-31 18:00:35 +0200

    fix hyprpaper

--- homemanager/gui/hyprpaper.nix
+++ homemanager/gui/hyprpaper.nix
@@ -1,21 +1,14 @@
 {...}: {
   services.hyprpaper = {
     enable = true;
-    settings = let
-      wp = "~/Pictures/wallpapers/artemis.jpg";
-    in {
-      ipc = "off";
-      preload = [
-        "${wp}"
-      ];
-      splash = "false";
-      wallpaper = [
-        "eDP-1,${wp}"
-        "DP-1,${wp}"
-        "DP-2,${wp}"
-        "HDMI-A-1,${wp}"
-        "HDMI-A-2,${wp}"
-      ];
+    settings = {
+      ipc = false;
+      splash = false;
+      wallpaper = {
+        monitor = "";
+        path = "~/Pictures/wallpapers/artemis.jpg";
+        fit_mode = "cover";
+      };
     };
   };
   wayland.windowManager.hyprland.settings.exec-once = ["hyprpaper"];
